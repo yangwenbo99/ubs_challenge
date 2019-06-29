281f414db0ebd8a9bfd72285ddac224f4afe20a4
Paul Yang <yangwenbo99@gmail.com>
2019-06-29 21:20:42 +0800

    i

---
+++ main.js
@@ -0,0 +1,67 @@
+"use strict;"
+
+function FormDescriptor() {
+	this.formsLoaded = [];
+	this.formsToLoad = ['basic_info'];
+	this.pageDescriptors = [];
+	this.validate = function () {
+		for (var f in this.pageDescriptors) {
+			if (this.pageDescriptors[f].validate && 
+				!this.pageDescriptors[f].validate()) {
+				$( '#validate_info' ).html(
+						this.pageDescriptors[f].validateInfo
+				)
+				return false;
+			}
+		}
+		$( '#validate_info' ).html("")
+		return true;
+	};
+
+	this.getPath = function (name) {
+		return name + '.html';
+	};
+
+	this.addNewForm = function (forms) {
+		for (i in forms) {
+			if (!(this.formsLoaded.includes(forms[i])) && 
+				!(this.formsToLoad.includes(forms[i]))) {
+				this.formsToLoad.push(forms[i]);
+			}
+		}
+	}
+
+	this.loadNext = function () {
+		if (!this.validate()) {
+			return false;
+		}
+
+		for (var f in this.pageDescriptors) {
+			if (this.pageDescriptors[f].getRequires) {
+				this.addNewForm(
+					this.pageDescriptors[f].getRequires());
+			}
+		}
+
+		if (this.formsToLoad.length == 0) {
+			console.log("All loaded");
+			return false;
+		}
+		var name = this.formsToLoad.pop();
+		var pathName = this.getPath(name);
+		console.log("Loading " + pathName);
+		this.formsLoaded.push(name);
+		var elementId = name + '_outer';
+		$( '<div />', {
+			id: elementId
+		}).appendTo( '#main_form' );
+		$( '#' + elementId ).load(pathName);
+		/*
+		$.get("page.html", function(data) {
+			var data = $(data);
+			$("#div").html(data);
+		});
+		*/
+		return true;
+	};
+}
